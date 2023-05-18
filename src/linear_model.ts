164bc2a471cd7fa53d805be5ee5798a460d67002
Aryan Suri <arysuri@proton.me>
2023-05-17 18:58:34 -0700

    linear model

---
+++ src/linear_model.ts
@@ -0,0 +1,34 @@
+export const TRAINING_SET = [
+  [1, 2],
+  [2, 4],
+  [4, 8],
+  [10, 20],
+];
+
+export function linear_model() {
+  const epsilon = 1e-3;
+  const k = 1e-3;
+  const gen = 500;
+  let w = 7.5;
+
+  function cost(w: number) {
+    let cost = 0;
+    for (let i = 0; i < TRAINING_SET.length; i++) {
+      const x: number = TRAINING_SET[i][0];
+      const f: number = x * w;
+      const expected: number = TRAINING_SET[i][1];
+      const distance: number = f - expected;
+      cost += distance * distance;
+    }
+    return cost / TRAINING_SET.length;
+  }
+
+  for (let i = 0; i < gen; i++) {
+    console.log(cost(w));
+    const dCost = (cost(w + epsilon) - cost(w)) / epsilon;
+    w -= k * dCost;
+    console.log(cost(w));
+  }
+}
+
+linear_model();
