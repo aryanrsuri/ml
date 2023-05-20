0f6370e90bafe66f6367b65019b41626fc80ca70
Aryan Suri <arysuri@proton.me>
2023-05-19 21:32:57 -0700

    documentaiotn

--- lib/activations.ts
+++ lib/activations.ts
@@ -1,6 +1,17 @@
+/**
+ * Bound output signal to sigmoid
+ * @param {number} x signal
+ * @returns {number} sigmoid(x) R [0, 1]
+ */
 export const sigmoid = (x: number): number => {
   return 1 / (1 + Math.exp(-x));
 };
+
+/**
+ * Bound output signal to rectified linear unit (ReLu)
+ * @param {number} x signal
+ * @returns {number} x < 0: 0 ? x
+ */
 
 export const ReLu = (x: number): number => {
   if (x < 0) {
@@ -9,6 +20,12 @@ export const ReLu = (x: number): number => {
   return x;
 };
 
+/**
+ * Bound output signal to leaky rectified linear unit (LeReLu)
+ * @param {number} x signal
+ * @returns {number} x < 0: exp(x) ? x
+ */
+
 export const LeReLu = (x: number): number => {
   if (x < 0) {
     return Math.exp(x);
@@ -16,9 +33,21 @@ export const LeReLu = (x: number): number => {
   return x;
 };
 
+/**
+ * Bound output signal to hyperbolic tangent function
+ * @param {number} x signal
+ * @returns {number} tanh(x) R [-1, 1]
+ */
+
 export const tanh = (x: number): number => {
   return Math.tanh(x);
 };
+
+/**
+ * Computes probability distribution of signal vector
+ * @param {number} v signal vector
+ * @returns {number} (exp(x) / sum^K_0(exp(x)))
+ */
 
 export const softmax = (v: number[]): number[] => {
   const prob_vector: number[] = [];
@@ -33,4 +62,3 @@ export const softmax = (v: number[]): number[] => {
 
   return prob_vector;
 };
-
